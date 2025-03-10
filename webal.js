50ef8bc931877285bbd47fd2bc4a9cd43b653933
Afficionado7 <150847865+Afficionado7@users.noreply.github.com>
2025-03-10 17:32:01 +0300

    Delete webal.js

--- webal.js
+++
@@ -1,7 +0,0 @@
-const help = document.querySelector('.help') ;
-const helps = document.querySelector('.helps') ;
-const opt = document.querySelector('.opt') ;
-
-opt.addEventListener('click', () => {
-    helps.classList.toggle('active');
-})
